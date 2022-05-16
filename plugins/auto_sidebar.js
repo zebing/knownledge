151f9393f9d4b7ec235efed17b6625ef728cbe76
zebing <1270105911@qq.com>
2022-05-16 18:46:05 +0800

    Create auto_sidebar.js

---
+++ plugins/auto_sidebar.js
@@ -0,0 +1,34 @@
+const glob = require('glob');
+
+module.exports = (options, ctx) => {
+  const slideTitle = ctx.themeConfig.slideTitle || {};
+  const paths = glob.sync('src/**/*');
+  const mdPaths = paths.filter((path) => /(?<!README)\.md$/.test(path));
+  const slideConfig = mdPaths.reduce((obj, path) => {
+    const pathArr = path.replace('src/', '').split('/');
+    pathArr[0] = `/${pathArr[0]}/`;
+    if (pathArr[pathArr.length - 1] === 'index.md') {
+      pathArr[pathArr.length - 1] = ''
+    }
+
+    if (!obj[pathArr[0]]) {
+      obj[pathArr[0]] = [];
+    }
+
+    let item = obj[pathArr[0]].filter((item) => item.key === pathArr[1])[0];
+
+    if (!item) {
+      item = {
+        title: slideTitle[pathArr[1]] || pathArr[1],
+        collapsable: true,
+        key: pathArr[1],
+        children: []
+      }
+      obj[pathArr[0]].push(item);
+    }
+
+    item.children.push(pathArr.slice(1).join('/'));
+    return obj;
+  }, {})
+  ctx.themeConfig.sidebar = slideConfig;
+}
